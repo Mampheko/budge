f9126d3416d15dc22884d6ddc2b1003b7bf16cb6
Tanya <keratilemampheko20@gmail.com>
2022-08-10 09:54:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState } from 'react'
+import AddItem from './components/addItem'
+import {DisplayTransaction} from './components/displayTransaction';
+
+function App() {
+
+  const [transaction, setTransaction] = useState([]);
+
+  const addTransaction = ((amount, item , transactionType) => {
+
+    setTransaction((item) => [...item, {
+      amount:amount,
+      item:item,
+      transactionType: transactionType
+    }])
+
+  })
+  return (
+    <div className="container">
+      <DisplayTransaction list={transaction} />
+      <AddItem add={addTransaction} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/addItem.js
@@ -0,0 +1,41 @@
+
+import React,{useState} from "react"
+import "../css/add.css";
+function AddItem(props) {
+
+    const [amount, setAmount] = useState('')
+    const [item, setItem] = useState("")
+    const [transactionType, settransactionType] = useState("")
+
+    const add = (()=>{
+        console.log(item)
+        console.log(amount);
+        console.log(transactionType);
+    
+        props.add(amount, item, transactionType);
+    })
+    return (
+        <div>
+
+            <h1 style={{paddingTop:'5px'}}>Add Transactions</h1>
+            <input
+                placeholder="Enter item"
+                onChange={(e) => setItem(e.target.value)}
+            />{" "}
+            <br></br>
+            <input
+                placeholder="Enter amount"
+                onChange={(e) => setAmount(e.target.value)}
+            /> {" "}
+            <br></br>
+            <select onChange={(e) => settransactionType(e.target.value)}>
+                <option value="Income">Income</option>
+                <option valu="Expense">Expense</option>
+            </select> {" "}
+            <br></br>
+            <button id="btn" onClick={add}>add</button>
+        </div>
+    )
+}
+
+export default AddItem;
